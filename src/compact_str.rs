fc91fc96f882e6c90fbf01e38742df526b000fd2
Fuu <g.for.xyz@gmail.com>
2025-07-31 15:19:53 +0800

    add unit-tests for compact_str
    Signed-off-by: Fuu <g.for.xyz@gmail.com>

--- src/compact_str.rs
+++ src/compact_str.rs
@@ -102,7 +102,7 @@ impl<const N: usize> CompactString<N> {
             len = *self.second_usize();
             #[cfg(target_endian = "little")]
             {
-                len &= 0x00_FF_FF_FF__FF_FF_FF_FF;
+                len &= 0x00FF_FFFF_FFFF_FFFF;
             }
             #[cfg(target_endian = "big")]
             {
@@ -158,7 +158,7 @@ impl<const N: usize> Drop for CompactString<N> {
 
             #[cfg(target_endian = "little")]
             {
-                len &= 0x00FF_FF_FF_FF_FF_FF_FF;
+                len &= 0x00FF_FFFF_FFFF_FFFF;
             }
             #[cfg(target_endian = "big")]
             {
@@ -170,5 +170,177 @@ impl<const N: usize> Drop for CompactString<N> {
                 let _ = Box::from_raw(slice_ptr);
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::thread;
+
+    fn gen_str(len: usize) -> String {
+        "a".repeat(len)
+    }
+
+    #[test]
+    fn test_empty_string() {
+        let cs_new = CompactString::<32>::new("");
+        assert_eq!(cs_new.as_str(), "");
+        assert_eq!(cs_new.as_slice().len(), 0);
+        assert_eq!(
+            *cs_new.last_byte(),
+            CompactString::<32>::LAST_BYTE_FIRST_INVALID_UTF8,
+            "Empty string from new() should have tag 192"
+        );
+
+        let cs_const = CompactString::<32>::EMPTY;
+        assert_eq!(cs_const.as_str(), "");
+        assert_eq!(cs_const.as_slice().len(), 0);
+        assert_eq!(
+            *cs_const.last_byte(),
+            CompactString::<32>::LAST_BYTE_FIRST_INVALID_UTF8,
+            "EMPTY constant should have tag 192"
+        );
+
+        assert_eq!(cs_new.bytes, cs_const.bytes);
+    }
+
+    #[test]
+    fn test_stack_storage_len_less_than_n() {
+        const N: usize = 24;
+        let s = "hello world"; // len = 11
+        let cs = CompactString::<N>::new(s);
+
+        assert_eq!(cs.as_str(), s);
+        assert_eq!(cs.as_slice().len(), s.len());
+        assert_eq!(*cs.last_byte(), 192 + s.len() as u8);
+    }
+
+    #[test]
+    fn test_stack_storage_len_equals_n() {
+        const N: usize = 32;
+        let s = gen_str(N);
+        let cs = CompactString::<N>::new(&s);
+
+        assert_eq!(cs.as_str(), s.as_str());
+        assert_eq!(cs.as_slice().len(), N);
+        assert_eq!(*cs.last_byte(), b'a');
+    }
+
+    #[test]
+    fn test_stack_storage_len_equals_n_with_multibyte_char() {
+        const N: usize = 32;
+        let s = gen_str(N - 3) + "€";
+        assert_eq!(s.len(), N);
+
+        let cs = CompactString::<N>::new(&s);
+
+        assert_eq!(cs.as_str(), s.as_str());
+        assert_eq!(cs.as_slice().len(), N);
+        assert_eq!(*cs.last_byte(), 0xAC);
+    }
+
+    #[test]
+    fn test_heap_storage() {
+        const N: usize = 20;
+        let s = gen_str(N + 1);
+        let cs = CompactString::<N>::new(&s);
+
+        assert_eq!(cs.as_str(), s.as_str());
+        assert_eq!(cs.as_slice().len(), s.len());
+        assert_eq!(*cs.last_byte(), CompactString::<N>::LAST_BYTE_HEAP);
+    }
+
+    #[test]
+    fn test_static_storage() {
+        const N: usize = 16;
+        const STATIC_STR: &'static str = "this is a static string literal that is long";
+        let cs = CompactString::<N>::new_static(STATIC_STR);
+
+        assert_eq!(cs.as_str(), STATIC_STR);
+        assert_eq!(cs.as_slice().len(), STATIC_STR.len());
+        assert_eq!(*cs.last_byte(), CompactString::<N>::LAST_BYTE_STATIC);
+        assert_eq!(*cs.first_usize(), STATIC_STR.as_ptr() as usize);
+    }
+
+    #[test]
+    fn test_static_empty_string() {
+        let cs = CompactString::<16>::new_static("");
+        assert_eq!(cs.as_str(), "");
+        assert_eq!(
+            *cs.last_byte(),
+            CompactString::<16>::LAST_BYTE_FIRST_INVALID_UTF8
+        );
+    }
+
+    #[test]
+    fn test_n_boundary_conditions_min() {
+        const N: usize = 16;
+
+        // len < N
+        let s15 = gen_str(15);
+        let cs15 = CompactString::<N>::new(&s15);
+        assert_eq!(cs15.as_str(), s15.as_str());
+        assert_eq!(*cs15.last_byte(), 192 + 15);
+
+        // len == N
+        let s16 = gen_str(16);
+        let cs16 = CompactString::<N>::new(&s16);
+        assert_eq!(cs16.as_str(), s16.as_str());
+        assert_eq!(*cs16.last_byte(), b'a');
+
+        // len > N
+        let s17 = gen_str(17);
+        let cs17 = CompactString::<N>::new(&s17);
+        assert_eq!(cs17.as_str(), s17.as_str());
+        assert_eq!(*cs17.last_byte(), CompactString::<N>::LAST_BYTE_HEAP);
+    }
+
+    #[test]
+    fn test_n_boundary_conditions_max() {
+        const N: usize = 61;
+
+        // len < N
+        let s60 = gen_str(60);
+        let cs60 = CompactString::<N>::new(&s60);
+        assert_eq!(cs60.as_str(), s60.as_str());
+        assert_eq!(*cs60.last_byte(), 192 + 60);
+
+        // len == N
+        let s61 = gen_str(61);
+        let cs61 = CompactString::<N>::new(&s61);
+        assert_eq!(cs61.as_str(), s61.as_str());
+        assert_eq!(*cs61.last_byte(), b'a');
+
+        // len > N
+        let s62 = gen_str(62);
+        let cs62 = CompactString::<N>::new(&s62);
+        assert_eq!(cs62.as_str(), s62.as_str());
+        assert_eq!(*cs62.last_byte(), CompactString::<N>::LAST_BYTE_HEAP);
+    }
+
+    #[test]
+    fn test_drop_heap_string_does_not_panic() {
+        let handle = thread::spawn(|| {
+            let s = gen_str(100);
+            for _ in 0..1000 {
+                let cs = CompactString::<32>::new(&s);
+                assert_eq!(*cs.last_byte(), CompactString::<32>::LAST_BYTE_HEAP);
+            }
+        });
+        handle.join().unwrap();
+    }
+
+    #[test]
+    fn test_drop_static_string_does_not_panic() {
+        const STATIC_STR: &'static str = "I must live forever";
+        let handle = thread::spawn(move || {
+            {
+                let cs = CompactString::<16>::new_static(STATIC_STR);
+                assert_eq!(*cs.last_byte(), CompactString::<16>::LAST_BYTE_STATIC);
+            } // cs is dropped here
+        });
+        handle.join().unwrap();
+        assert_eq!(STATIC_STR, "I must live forever");
     }
 }
